8e34faeaef24c62654e1375dbe1c0cf87b1fb640
Hamaguri181 <ri.yuihama@gmail.com>
2026-04-01 14:07:27 +0900

    CountryPukiwikiFormatterの解放・形成可能の判定方法をFrozenSetに変更

--- Victoria3.Formatting/PukiwikiFormatters/CountryPukiwikiFormatter.cs
+++ Victoria3.Formatting/PukiwikiFormatters/CountryPukiwikiFormatter.cs
@@ -5,7 +5,7 @@ using Victoria3.Localization;
 
 namespace Victoria3.Formatting.PukiwikiFormatters
 {
-    public class CountryPukiwikiFormatter()
+    public sealed class CountryPukiwikiFormatter()
     {
         public string Format(
             IEnumerable<Country> items,
@@ -20,10 +20,12 @@ namespace Victoria3.Formatting.PukiwikiFormatters
             sb.AppendLine("|||||50|65|||||35|35|35|c");
             sb.AppendLine("||タグ|>|国名|種別|ティア|主要文化|国教((第一主要文化の文化宗教と異なる場合のみ記載))|ヒエラルキー|首都|初期存在|解放可能|形成可能|h");
 
-            var initialCountries = historicalStateRegions
+            var initialTags = historicalStateRegions
                 .SelectMany(hsr => hsr.CreateStates)
                 .Select(cs => RemovePrefix(cs.Country))
                 .ToFrozenSet();
+            var releasableTags = releasableCountries.Select(rc => rc.Tag).ToFrozenSet();
+            var formableTags = formableCountries.Select(fc => fc.Tag).ToFrozenSet();
             foreach (var country in items)
             {
                 var englishName = englishLocalizer.Localize(country.Tag);
@@ -34,9 +36,9 @@ namespace Victoria3.Formatting.PukiwikiFormatters
                 var religion = localizer.Localize(country.Religion);
                 var hierarchy = localizer.Localize(country.SocialHierarchy);
                 var capital = localizer.Localize(country.Capital);
-                var isInitial = initialCountries.Contains(country.Tag) ? "初期" : "";
-                var isReleasable = releasableCountries.Any(rc => rc.Tag == country.Tag) ? "解放" : "";
-                var isFormable = formableCountries.Any(fc => fc.Tag == country.Tag) ? "形成" : "";
+                var isInitial = initialTags.Contains(country.Tag) ? "初期" : "";
+                var isReleasable = releasableTags.Contains(country.Tag) ? "解放" : "";
+                var isFormable = formableTags.Contains(country.Tag) ? "形成" : "";
 
                 sb.AppendLine($"|BGCOLOR({country.Color.ToColorCode()}):|~{country.Tag}|{englishName}|{name}|{countryType}|{tier}|{cultures}|{religion}|{hierarchy}|{capital}|{isInitial}|{isReleasable}|{isFormable}|");
             }
