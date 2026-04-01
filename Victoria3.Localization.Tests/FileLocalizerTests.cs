ac4746549b12bcfbc14626bb0fd27255f30f9ca6
Hamaguri181 <ri.yuihama@gmail.com>
2026-04-01 14:52:11 +0900

    Localizerのnullと接頭辞に対する仕様変更に対応してテストを修正

--- Victoria3.Localization.Tests/FileLocalizerTests.cs
+++ Victoria3.Localization.Tests/FileLocalizerTests.cs
@@ -19,5 +19,35 @@ namespace Victoria3.Localization.Tests
 
             localizer.TryLocalize("unknown", out var _).Should().BeFalse();
         }
+
+        [Fact(DisplayName = "キーがnullの場合、Localizeは空文字列を返す")]
+        public void Localize_NullKey_ReturnsEmptyString()
+        {
+            var localizer = FileLocalizer.FromText("""
+                known:0 "known-value"
+                """);
+
+            localizer.Localize(null).Should().Be(string.Empty);
+        }
+
+        [Fact(DisplayName = "キーがnullの場合、TryLocalizeはfalseを返す")]
+        public void TryLocalize_NullKey_ReturnsFalse()
+        {
+            var localizer = FileLocalizer.FromText("""
+                known:0 "known-value"
+                """);
+            localizer.TryLocalize(null, out var _).Should().BeFalse();
+        }
+
+        [Fact(DisplayName = "キーに接頭辞がある場合、接頭辞を削除してローカライズされる")]
+        public void Localize_KeyWithPrefix_PrefixRemoved()
+        {
+            var localizer = FileLocalizer.FromText("""
+                known:0 "known-value"
+                """);
+            localizer.Localize("prefix:known").Should().Be("known-value");
+            localizer.TryLocalize("prefix:known", out var value).Should().BeTrue();
+            value.Should().Be("known-value");
+        }
     }
 }
